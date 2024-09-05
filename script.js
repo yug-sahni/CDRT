f0314eac879ce1bb73bbe84ae484e01538231d29
yug-sahni <yugsahni2003@gmail.com>
2024-09-05 18:33:47 +0530

    latitude-longitude

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ const items = [
 const itemList = document.getElementById('itemList');
 const selectedItem = document.getElementById('selectedItem');
 
+function getUserLocation() {
+    // Check if Geolocation is supported
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    
+    // const locationBox = document.getElementById('locationBox');
+    // locationBox.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            alert("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            alert("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            alert("An unknown error occurred.");
+            break;
+    }
+}
+getUserLocation();
+
 items.forEach(item => {
     const listItem = document.createElement('div');
     listItem.textContent = item;
